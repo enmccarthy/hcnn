426704dbdcd7e21bc4e8a115b86f3551d2ef9a09
Erin McCarthy <erinmccarthy@dyna6-78.cs.uoregon.edu>
2018-05-15 15:31:07 -0700

    finished forward propagation

--- neuralNet/src/Lib.hs
+++ neuralNet/src/Lib.hs
@@ -2,13 +2,13 @@ module Lib where
 
 import qualified Numeric.LinearAlgebra as NL
 import qualified Data.Vector as DV
-import System.Random 
+import System.Random
 import Data.Functor
-import Control.Monad 
+import Control.Monad
 
 data Layer = Input Int
     | Output Int
-    | Hidden Int 
+    | Hidden Int
 -- weight ish
 -- takes in an activation f weights bias and inputs
 -- weight :: (Float -> Float) -> Vector Float -> Float -> Vector Float
@@ -27,12 +27,12 @@ mse newy oldy =  ((sum [(i-j) | i <- oldy,
 --     exps = np.exp(X-np.max(X))
 --     return exps / np.sum(exps)
 softmax :: (DV.Vector Float) -> (DV.Vector Float)
-softmax x = 
+softmax x =
     (DV.map (/(DV.sum toX)) toX)
 
     where toX = (DV.map exp (DV.map (subtract (DV.maximum x)) x))
- 
-                  
+
+
 --cross entropy loss
 -- python code:
 -- X is the output from fully connected layer (num_examples x num_classes)
@@ -51,7 +51,7 @@ cel x y = (DV.sum (DV.map (\(pi,yi) -> -yi*(log pi)) zip1)) / (fromIntegral len)
           len  = (length y)
 
 -- telling how I want them zipped, idk what will happen if they are different size
--- so if I have issues come back and look at this 
+-- so if I have issues come back and look at this
 zipTheseV :: Float -> Float -> (Float, Float)
 zipTheseV a b = (a , b)
 
@@ -61,22 +61,22 @@ zipTheseV a b = (a , b)
 relu :: Float -> Float
 relu = max 0
 
-type InputLayers = [Layer] 
+type InputLayers = [Layer]
 -- I need to keep track of the nodes in the previous layer
 -- There is probably a better way to do this
-type Model = [(Int, (IO (DV.Vector Float), IO[(DV.Vector Float)]))]
+type Model = [(Int, (IO Float, IO[(DV.Vector Float)]))]
 
 -- our initial weights and bias as well as number of layers
 
 
--- I make the assumption that input comes first and 
+-- I make the assumption that input comes first and
 -- output comes last but maybe I want to put a check somewhere
 -- for that
 
--- takes a standard dev as input 
+-- takes a standard dev as input
 -- grabbed from the internet
--- there are packages that have this implemented but 
--- I had problems downloading them 
+-- there are packages that have this implemented but
+-- I had problems downloading them
 -- this is boxmuller
 gauss :: Float -> IO Float
 gauss scale = do
@@ -87,26 +87,48 @@ gauss scale = do
 initM :: Model -> InputLayers -> (Model, InputLayers)
 initM wholemod@((num, (b, w)):ms) (ls:lay) = case ls of
                 (Input numIn) -> ([(numIn, (b,w))], lay)
-                (Output numOut) -> ((reverse ([(numOut, ((createB numOut), (createW num numOut)))] ++ wholemod)), []) -- create the bias and weights for output layer 
-                (Hidden numHid) -> (([(numHid, ((createB numHid), (createW num numHid)))] ++ wholemod), lay)
+                (Output numOut) -> ((reverse
+                                      ([(numOut,
+                                        ((gauss (0.01)), (createW num numOut)))]
+                                            ++ wholemod))
+                                              , []) -- create the bias and weights for output layer
+                (Hidden numHid) -> (([(numHid,
+                                      ((gauss (0.01)), (createW num numHid)))]
+                                          ++ wholemod)
+                                            , lay)
 
+
+-- did this wrong so maybe come back to this to make it prettier
 --possibly a more efficient way than fromList
-createB :: Int -> IO(DV.Vector Float)
-createB num = do
+createWHelp :: Int -> IO(DV.Vector Float)
+createWHelp num = do
             listRan <- (replicateM num (gauss (0.01)))
             return (DV.fromList listRan)
 createW :: Int -> Int -> IO[(DV.Vector Float)]
-createW num num2 = (replicateM num (createB num2))
+createW num num2 = (replicateM num (createWHelp num2))
 
 
 -- forward prop
--- input the model
--- matrix multiplication summation and activations function
-forwardprop :: Model -> (Dv.Vector Float) -> (DV.Vector Float)
-forwardprop ((_, (b, w)):ms) vec =   
+forwardprop :: Model -> (DV.Vector Float) -> IO (DV.Vector Float)
+forwardprop [] vec = return vec
+forwardprop ((_, (b, w)):ms) vec = do
+                                  weight <- w
+                                  bias <- b
+                                  (forwardprop ms (DV.fromList
+                                                  (map relu
+                                                  (map (+ bias)
+                                                  (map (DV.foldl1 (+))
+                                                  (map (DV.zipWith zipTheseProp vec)
+                                                        weight))))))
+
+
+zipTheseProp :: Float -> Float -> Float
+zipTheseProp a b = (a * b)
+
 
 -- backwards prop
+
 -- gradient descent
 
--- batch 
+-- batch
 -- online learning

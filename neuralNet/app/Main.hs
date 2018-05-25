3d6525173d374ad6b30c55aa3c91dcb8dc784bdf
Erin McCarthy <mccarthyern@gmail.com>
2018-05-25 13:07:02 -0700

    was having problems with main still debugging

--- neuralNet/app/Main.hs
+++ neuralNet/app/Main.hs
@@ -5,8 +5,11 @@ import Lib
 import qualified Data.ByteString.Lazy as BS
 import Data.Functor
 import Data.List
+import Data.Ord
+import Control.Monad
 -- import qualified Data.Vector as DV
 import System.Random
+    
 
 
 createMod :: InputLayers -> Model -> Model
@@ -16,46 +19,68 @@ createMod ip mod = let (currMod, currIp) = (initModel mod ip) in
 
 -- These came from https://crypto.stanford.edu/~blynn/haskell/brain.html
 getImage s n = fromIntegral . BS.index s . (n*28^2 + 16 +) <$> [0..28^2 - 1]
-
 getX     s n = (/ 256) <$> getImage s n
 getLabel s n = fromIntegral $ BS.index s (n + 8)
 getY     s n = fromIntegral . fromEnum . (getLabel s n ==) <$> [0..9]
 
-train 1 model trainI trainL = do
-    n <- (`mod` 10000) <$> randomIO
-    let vectorExample = getX trainI n
-    let vectorAns = getY trainL n
-    (vec, err) <- (forwardprop model (vectorExample, []))
-    newMod <- (backwardsprop (reverse model) vec vectorAns (reverse err))
-    return (reverse newMod)
-train iter model trainI trainL = do
-    n <- (`mod` 10000) <$> randomIO
-    let vectorExample = getX trainI n
-    let vectorAns = getY trainL n
-    (vec, err) <- (forwardprop model (vectorExample, []))
-    newMod <- (backwardsprop (reverse model) vec vectorAns (reverse err))
-    train (iter-1) (reverse newMod) trainI trainL
+-- train the model with given test sets -- 
+-- takes in a number of iterations a model and the training set and solutions
+-- train :: Int -> [([Float], [[Float]])] -> BS.ByteString -> BS.ByteString -> [([Float], [[Float]])] 
+-- train 1 model trainSet trainKey = 
+--     let 
+--         vectorExample = (getX trainSet 1)
+--         vectorAns     = (getY trainKey 1)
+--         (vec, err)    = (forwardprop model (vectorExample, [])) in
+--     (backwardsprop (reverse model) vec vectorAns (reverse err))
 
-test 0 model testI testL correct total = do
-    let vectorExample = getX testI 0
-    let vectorAns = getY testL 0
-    (vector, error) <- (forwardprop model (vectorExample, []))
-    let newtotal = (total+1)
-    if ((elemIndex (maximum vector) vector) == (elemIndex (maximum vectorAns) vectorAns))
-        then return ((correct+1)/newtotal)
-        else return (correct/newtotal)
+-- train iter model trainSet trainKey =
+--     let 
+--         vectorExample = (getX trainSet iter)
+--         vectorAns     = (getY trainKey iter)
+--         (vec, err)    = (forwardprop model (vectorExample, []))
+--         newMod        = (backwardsprop (reverse model) vec vectorAns (reverse err)) in
+--     -- must reverse becasue backprop is backwards
+--     train (iter-1) (reverse newMod) trainSet trainKey
+
+-- test :: Int -> [([Float], [[Float]])] -> BS.ByteString -> BS.ByteString -> IO([([Float], [Integer])]) 
+-- test 0 model testSet testKey = do
+--     n <- (`mod` 1000) <$> randomIO
+--     let vectorExample = getX testSet n
+--     let vectorAns = getY testKey n 
+--     let (vector, error) = (forwardprop model (vectorExample, []))
+--     return ([(vector, vectorAns)])
+--     -- let newtotal = (total+1)
+
+--     -- if ((elemIndex (maximum vector) vector) == (elemIndex (maximum vectorAns) vectorAns))
+--     --     then return ((correct+1)/newtotal)
+--     --     else return (correct/newtotal)
     
-test iter model testI testL correct total = do
-    let vectorExample = getX testI iter
-    let vectorAns = getY testL iter
-    (vector, error) <- (forwardprop model (vectorExample, []))
-    let newtotal = (total+1)
-    if ((elemIndex (maximum vector) vector) == (elemIndex (maximum vectorAns) vectorAns)) 
-        then test (iter-1) model testI testL (correct+1) newtotal
-        else test (iter-1) model testI testL correct newtotal
-    
+-- test iter model testSet testKey = do
+--     n <- (`mod` 1000) <$> randomIO
+--     let vectorExample = getX testSet n
+--     let vectorAns = getY testKey n
+--     let (vector, error) = (forwardprop model (vectorExample, []))
 
+--     restofTest <- (test (iter-1) model testSet testKey)
+--     --returning the vector output and the answer
+--     return((vector, vectorAns):restofTest)
+    -- (print vectorAns)
+    -- if ((elemIndex (maximum vector) vector) == (elemIndex (maximum vectorAns) vectorAns)) 
+    --     then test (iter-1) model testSet testKey (correct+1) newtotal 
+    --     else test (iter-1) model testSet testKey correct newtotal
 
+-- convert model to not have the i, aka the number of previous nodes --
+convertMod ((i, (b,w)):[]) = do
+    bias <- b
+    weight <- w
+    return ([(bias, weight)])
+convertMod ((i,(b,w)):rm) = do
+    bias <- b
+    weight <- w
+    restMod <- (convertMod rm)
+    return ((bias, weight):restMod)
+
+render n = let s = " .:oO@" in s !! (fromIntegral n * length s `div` 256)
 
 main :: IO ()
 main = do
@@ -65,23 +90,49 @@ main = do
         ,  "./src/t10k-images-idx3-ubyte.gz"
         ,  "./src/t10k-labels-idx1-ubyte.gz"
         ]
+    -- create model and remove the first layer because it is not necessary --
+    let (m:model) = (createMod [(Input 784), (Hidden 30), (Output 10)] [(0, ((return [0.01]), (return [])))])
+    -- changed formats half way through -- 
+    b <- (convertMod model)
+    n <- (`mod` 10000) <$> randomIO
+    putStr . unlines $
+        take 28 $ take 28 <$> iterate (drop 28) (render <$> getImage testI n)
 
-    let (m:model) = (createMod [(Input 784), (Hidden 200), (Output 10)] [(0, ((return 0.01), (return [])))])
-    -- let testmod = [(2, (1.0), return [[4.0, 3.0], [4.0, 3.0]])), (2, ((return 1.0), return [[2.0, 3.0], [2.0, 3.0]]))]
-    trainedMod <- (train 10000 model trainI trainL)
-    let vectorExample = getX testI 1
-    let vectorAns = getY testL 1
-    let testVector = [2.0, 1.0]
-    (vec, err) <- (forwardprop trainedMod (vectorExample, []))
-    -- (print vec)
-    -- (print err)
-    --(vector2, err) <- (forwardprop model (vectorExample, []))
-    (vector, error) <- (forwardprop model (vectorExample, []))
-    -- (print vector2)
-    -- (print vectorAns) 
-    total <- (test 9999 trainedMod testI testL 0 0)
-    (print total)
-    --(print err)
-    -- (print (cel [vector] vectorAns))
-    -- (print (elemIndex (maximum vectorAns) vectorAns))
+    let
+        example = getX testI n
+        bs = scanl (foldl' (\b n -> learn (getX trainI n) (getY trainL n) b)) b [
+            [   0.. 999],
+            [1000..2999],
+            [3000..5999],
+            [6000..9999]]
+        smart = last bs
+        cute d score = show d ++ ": " ++ replicate (round $ 70 * min 1 score) '+'
+        bestOf = fst . maximumBy (comparing snd) . zip [0..]
+
+    forM_ bs $ putStrLn . unlines . zipWith cute [0..9] . feed example
+
+    putStrLn $ "best guess: " ++ show (bestOf $ feed example smart)
+
+    let guesses = bestOf . (\n -> feed (getX testI n) smart) <$> [0..9999]
+    let answers = getLabel testL <$> [0..9999]
+    -- train the model --
+    -- let vectorExample = getX trainI 1
+    -- let vectorAns = getY trainL 2
+    -- let vectorExample2 = getX trainI 2
+    -- let vectorExample3 = getX trainI 3
+    -- let (vec, err) = (forwardprop otherFormat (vectorExample, []))
+    -- let (t:tmod) = (backwardsprop (reverse otherFormat) vec vectorAns (reverse err))
+    -- let trainedMod = (train 100 otherFormat trainI trainL)
+    --     newtrainedMod = trainedMod
+    -- -- testing the model -- 
+    -- let newtmod = (reverse tmod)
+    -- print (t)
+    -- print (vec)
+    -- let (vec2, err2) = (forwardprop (newtmod) (vectorExample2, []))
+    -- let (vec3, err3) = (forwardprop (reverse tmod) (vectorExample3, []))
+    -- (print vec2)
+    -- (print vec3)
+    -- total <- (test 3 newtrainedMod trainI trainL)
+    -- (print total)
+
     return ()
--- neuralNet/src/Lib.hs
+++ neuralNet/src/Lib.hs
@@ -5,6 +5,8 @@ import qualified Numeric.LinearAlgebra as NL
 import System.Random
 import Data.Functor
 import Control.Monad
+import Data.Ord
+import Data.List
 
 
 data Layer = Input Int
@@ -23,9 +25,10 @@ mse newy oldy =  (((sum [(i-j) | i <- oldy,
 
 softmax :: [Float] ->  [Float]
 softmax x =
-    (map (/(sum toX)) toX)
-
-    where toX = (map exp (map (subtract (maximum x)) x))
+    (map (/(summation)) toX)
+    where 
+        toX = (map exp (map (subtract (maximum x)) x))
+        summation = (sum (map exp (map (subtract (maximum x)) x)))
 
 
 --cross entropy loss
@@ -58,7 +61,7 @@ relu = max 0
 type InputLayers = [Layer]
 -- I need to keep track of the nodes in the previous layer
 -- There is probably a better way to do this
-type Model = [(Int, (IO Float, IO[[Float]]))]
+type Model = [(Int, (IO [Float], IO[[Float]]))]
 
 type Error = [([Float], [Float])]
 
@@ -81,53 +84,53 @@ initModel wholemod@((num, (b, w)):ms) (ls:lay) =
     (Input numIn)   -> ([(numIn, (b,w))], lay)
     (Output numOut) -> ((reverse
                           ((numOut,
-                            ((gauss (0.01)), (createW numOut num))):wholemod))
+                            ((createB numOut), (createW numOut num))):wholemod))
                                 , []) -- create the bias and weights for output layer
     (Hidden numHid) -> (((numHid,
-                          ((gauss (0.01)), (createW numHid num))):wholemod)
+                          ((createB numHid), (createW numHid num))):wholemod)
                                 , lay)
 
 -- possibly a more efficient way to do this--
 -- but wanted to make sure it gives different ran numbers
 -- I need to check that it does
+createB :: Int -> IO[Float]
+createB bin = return (replicate bin 1.0)
 createW :: Int -> Int -> IO[[Float]]
 createW num num2 = (replicateM num (createWHelp num2))
 
 createWHelp :: Int -> IO [Float]
-createWHelp num = do
-    -- listRan <- (replicateM num (gauss (0.01)))
-    (replicateM num (gauss (0.01)))
+createWHelp num = (replicateM num (gauss (0.01)))
 
 
 -- forward prop
 
-forwardprop :: Model -> ([Float], Error)
-                        -> IO([Float], Error)
-forwardprop [] (vec, err) = return (vec, err)
-forwardprop ((_, (b, w)):[]) (vec, err) = do
-    weight <- w
-    bias <- b
-    let output = (map (+ bias)
+forwardprop :: ([([Float], [[Float]])]) -> ([Float], Error)
+                        -> ([Float], Error)
+forwardprop [] (vec, err) = (vec, err)
+forwardprop ((bias, weight):[]) (vec, err) =
+    let output = (zipWith (+) bias
                     (map (foldl1 (+))
                     (multWeight (zipMultWeight vec
-                    weight))))
-    return ((softmax output), ([(output, (softmax output))] ++ err))
+                    weight)))) in 
+    ((softmax output), ([(output, (softmax output))] ++ err))
 
-forwardprop ((_, (b, w)):ms) (vec, err) = do
-    weight <- w
-    bias <- b
-    let output = (map (+ bias)
+forwardprop ((bias, weight):ms) (vec, err) =
+    let 
+        output = (zipWith (+) bias
                     (map (foldl1 (+))
                     (multWeight (zipMultWeight vec
-                        weight))))
-    (forwardprop ms ((map relu output), ((output, (map relu output)):err)))
+                        weight)))) 
+        newerror = if (err == []) then ((output, (map relu output)):[(vec, vec)])
+                    else ((output, (map relu output)):err) in 
+    (forwardprop ms ((map relu output), newerror))
 
+-- multiplying the first of every list together -- 
 zipMultWeight :: [Float] -> [[Float]] -> [([Float],[Float])]
 zipMultWeight a [] = []
 zipMultWeight a (b:bs) = [(a,b)] ++ (zipMultWeight a bs)
 
 multWeight [] = []
-multWeight ((a,b):rs) = [(zipWith zipMult a b)] ++ (multWeight rs)
+multWeight ((a,b):rs) = [(zipWith (*) a b)] ++ (multWeight rs)
 
 zipMult :: Float -> Float -> Float
 zipMult a b = (a * b)
@@ -136,12 +139,12 @@ zipMult a b = (a * b)
 -- backwards prop
 --the derivative of cross entropy
 -- takes in the y vector and the output vector
---CHECKED
+-- if statements so that it doesnt return nAn 
 derCE :: [Float] -> [Float] -> [Float]
 derCE yvec outvec = (map (* (-1)) (zipWith zipAdd
-                      (zipWith zipMult yvec (map (1/) outvec))
+                      (zipWith zipMult yvec (map (1/) (map (\x3 -> if x3 == 0 then 0.0000001 else x3) outvec)))
                       (zipWith zipMult (map (\x1 -> 1.0 - x1) yvec)
-                            (map (1.0/) (map (\x -> 1.0 - x) outvec)))))
+                            (map (1.0/) (map (\x -> 1.0 - x) (map (\x2 -> if x2 == 1 then 1.0000001 else x2)outvec))))))
 
 zipAdd :: Float -> Float -> Float
 zipAdd a b = (a + b)
@@ -159,27 +162,23 @@ derRelu :: [Float] -> [Float]
 derRelu inp = (map (\x -> if (x > 0) then 1 else 0) inp)
 
 
--- last layer to output--
--- derCE * derOut * layer before outvalue
-
-
 -- backwards prop
 -- TAKES A REVERSE MODEL/ERROR
 -- takes the y of the output of the model and the expected y
--- TODO: I think the errors are in the wrong order
-backwardsprop :: Model -> [Float]
-                          -> [Float] -> Error -> IO Model
-backwardsprop ((i, (b, w)):ms) out
-  expout err@((beforeSM, afterSM):(beforeRelu, afterRelu):re) = do
-      weight <- w
-      let dce = (derCE expout out) --(vector)
-      let dout = (derOut beforeSM) --(vector)
-      let changeWeight = (map (* 0.10)
+
+backwardsprop :: [([Float], [[Float]])] -> [Float]
+                          -> [Float] -> Error -> [([Float], [[Float]])]
+backwardsprop ((bias, weight):ms) output
+  expout ((beforeSM, afterSM):(beforeRelu, afterRelu):re) =
+      let 
+        dce          = (derCE expout output)
+        newbias      = (zipWith zipSub (map (*0.001) dce) bias)
+        dout         = (derOut beforeSM) 
+        changeWeight = (map (* 0.001)
                             (zipWith3 zipMult2 dce dout afterRelu))
-      let newWeight = return (map (zipWith zipSub changeWeight) weight)
-      otherMod <- (backprophelp ms dce dout weight err)
+        newWeight = (map (zipWith zipSub changeWeight) weight) in
       -- new output layer weights
-      return ((i, (b, newWeight)):otherMod)
+      ((newbias, newWeight):(backprophelp ms dce dout weight ((beforeRelu, afterRelu):re)))
 
 -- layer to layer
 -- derRelu * h1 outvalue * (derCE * derOut * (weights on 2nd layer output))
@@ -187,67 +186,38 @@ backwardsprop ((i, (b, w)):ms) out
 --input to layer
 --derRelu * input value out * (previous relu * previous 3rd value * weight coming into layer)
 
--- TODO: a lot of this is repetitive, pull it out and create a helper
--- this is also good for testing, right now it is this way because of
--- dealing with the I/O
+condense :: [[Float]] -> [Float]
+condense (x:xs) = condenseHelp xs x
 
-backprophelp :: Model -> [Float] ->  [Float]
-                    -> [ [Float]] -> Error -> IO Model
-backprophelp ((i, (b, w)):[]) d1 d2 weigh
-  ((h2input, h2output):(h1input, h1output):re) = do
-      currWeight <- w
-      let thirdValue = (map (zipWith3 zipMult2 d1 d2) weigh) --[vector]
-      let weightChange = (map (map (* 0.10))
-                            (map (zipWith3 zipMult2
-                                      (derRelu h2input) h1output) thirdValue))
-      let newWeight = return (zipWith zipVecSub weightChange currWeight)
-      return [(i, (b, newWeight))]
+condenseHelp [] vec     = vec 
+condenseHelp (m:ms) vec = condenseHelp ms (zipWith (+) m vec) 
 
-backprophelp ((i, (b, w)):ms) d1 d2 weigh
-  ((h2input, h2output):(h1input, h1output):re) = do
-      currWeight <- w
-      let thirdValue = (map (zipWith3 zipMult2 d1 d2) weigh)
-      let weightChange = (map (map (* 0.10))
-                          (map (zipWith3 zipMult2
-                              (derRelu h2input) h1output) thirdValue))
-      let newWeight = return (zipWith zipVecSub weightChange currWeight)
-      otherMod <- (backprophelp1 ms (derRelu h2input)
-                    thirdValue currWeight ((h1input, h1output):re))
-      return ((i, (b, newWeight)):otherMod)
+backprophelp :: [([Float], [[Float]])] -> [Float] ->  [Float]
+                    -> [ [Float]] -> Error -> [([Float], [[Float]])]
 
+backprophelp ((bias, currWeight):[]) d1 d2 weigh
+  ((h2input, h2output):(h1input, h1output):re) =
+      let 
+        newbias     = (zipWith zipSub (map (*0.002) d1) bias) 
+        thirdValue  = (condense (map (zipWith (*) (zipWith (*) d1 d2)) weigh))
+        weightChange = (map (* 0.002)
+                            (zipWith3 zipMult2
+                                      (derRelu h2input) h1output thirdValue))
+        newWeight = (map (zipWith zipSub weightChange) currWeight) in
+      [(newbias, newWeight)]
 
-backprophelp1 :: Model ->  [Float] -> [ [Float]]
-                      -> [ [Float]] -> Error -> IO Model
-backprophelp1 [] d1 d2 weigh _ = do
-    return []
+backprophelp ((bias, currWeight):ms) d1 d2 weigh
+  ((h2input, h2output):(h1input, h1output):re) =
+      let 
+        newbias      = (zipWith zipSub (map (*0.002) d1) bias)  
+        thirdValue   = (condense (map (zipWith3 zipMult2 d1 d2) weigh)) 
+        weightChange = (map (* 0.002)
+                          (zipWith3 (zipMult2)
+                              (derRelu h2input) h1output thirdValue))
+        newWeight    = (map (zipWith zipSub weightChange) currWeight)  in
+        ((newbias, newWeight):(backprophelp ms (derRelu h2input)
+                                thirdValue currWeight ((h1input, h1output):re)))
 
-backprophelp1 ((i, (b, w)):[]) d1 d2 weigh _ = do
-    return [(i, (b, w))]
-
-backprophelp1 ((i, (b, w)):(i1, (b1, w1)):[]) d1 d2 weigh
-  ((h2input, h2output):(h1input, h1output):re) = do
-      currWeight <- w
-      let maper = (map (zipWith zipMult d1) d2)
-      let thirdValue = (zipWith zipVec maper weigh)
-      let weightChange = (map (map (* 0.10))
-                              (map (zipWith3 zipMult2
-                                  (derRelu h2input) h1output) thirdValue))
-      let newWeight = return (zipWith zipVecSub weightChange currWeight)
-      return [(i, (b, newWeight))]
-
-backprophelp1 ((i, (b, w)):ms) d1 d2 weigh
-  ((h2input, h2output):(h1input, h1output):re) = do
-      currWeight <- w
-      let dRelu = (derRelu h2input)
-      let maper = (map (zipWith zipMult d1) d2)
-      let thirdValue = (zipWith zipVec maper weigh)
-      let weightChange = (map (map (* 0.10))
-                              (map (zipWith3 zipMult2
-                                dRelu h1output) thirdValue))
-      let newWeight = return (zipWith zipVecSub weightChange currWeight)
-      otherMod <- (backprophelp1 ms dRelu
-                    thirdValue currWeight ((h1input, h1output):re))
-      return ((i, (b, newWeight)):otherMod)
 
 --look to see where this is used and change it
 zipVec ::  [Float] ->  [Float] ->  [Float]
@@ -261,3 +231,14 @@ zipMult2 a b c= (a * b * c)
 
 zipSub :: Float -> Float -> Float
 zipSub a b = (b - a)
+
+
+learn x y layers = 
+    let
+        (vec, err) = (forwardprop layers (x, [])) in 
+    (backwardsprop (reverse layers) vec y (reverse err))
+
+feed vec layers = 
+    let
+        (vector, err) = (forwardprop layers (vec, [])) in
+    vector
